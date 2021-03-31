1bb84a688a74ba92d6a18c933de03e90d5885024
ilayda <ilaydasimsek4@gmail.com>
2021-03-31 18:40:25 +0300

    Created the project

---
+++ CrypTouchBar/CrypTouchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CrypTouchBar
+//
+//  Created by İlayda Şimşek on 31.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CrypTouchBar/CrypTouchBar/CrypTouchBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrypTouchBarApp.swift
+//  CrypTouchBar
+//
+//  Created by İlayda Şimşek on 31.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct CrypTouchBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
