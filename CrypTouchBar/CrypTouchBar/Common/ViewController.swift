9cfe04c3175b3e61f6bbf1bacf0cb7c14fa7d473
ilayda <ilaydasimsek4@gmail.com>
2021-04-14 20:54:55 +0300

    Changed the way we load views to a more logical structure

--- CrypTouchBar/CrypTouchBar/Common/ViewController.swift
+++ CrypTouchBar/CrypTouchBar/Common/ViewController.swift
@@ -1,12 +1,24 @@
 import Cocoa
 
 class NibViewController<T: NSView>: NSViewController {
-    
-    override var nibName: NSNib.Name? {
-        return T.nibIdentifier
-    }
 
     var rootView: T {
         return self.view as! T
+    }
+
+    override func loadView() {
+        guard let nibView = loadViewFromNib() else {
+            fatalError("Could not load nib with type \(T.nibIdentifier). Something might be wrong with the nib file.")
+        }
+        self.view = nibView
+    }
+
+    private func loadViewFromNib() -> T? {
+        var topLevelObjects: NSArray? = nil
+        Bundle.main.loadNibNamed(NSNib.Name(T.nibIdentifier), owner: self, topLevelObjects: &topLevelObjects)
+        guard let results = topLevelObjects,
+              let view = Array<Any>(results).filter({ $0 is T }).last as? T
+              else { return nil }
+        return view
     }
 }
